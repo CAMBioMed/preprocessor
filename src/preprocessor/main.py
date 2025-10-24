de633169e039eb3188dafb1b696a67dbb89fda6c
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2025-10-24 16:38:24 +0200

    Add example main window using Qt Widgets

--- src/preprocessor/main.py
+++ src/preprocessor/main.py
@@ -1,7 +1,8 @@
 import click
 import PySide6.QtCore
 from preprocessor._version import __version__
-from preprocessor.hello_world import show_ui
+# from preprocessor.hello_world import show_ui
+from preprocessor.main_window import show_ui
 
 
 @click.group()
