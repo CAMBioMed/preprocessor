b502f9525b56fd344884c15b461c9066301dadd0
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2025-11-04 10:54:52 +0100

    Fix build and test tasks

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,12 +1,12 @@
 from preprocessor._version import __version__  # type: ignore
 
 
-def test_add__two_integers():
+def test_add__two_integers() -> None:
     expected = 5
     result = 2 + 3
     assert result == expected, f"Expected {expected}, but got {result}."
 
 
-def test_print_version():
+def test_print_version() -> None:
     assert isinstance(__version__, str)
     assert len(__version__) > 0
