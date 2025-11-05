dc386fe780c8dd2bacd7ac7dd48cf0a22ee28183
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2025-11-05 16:10:24 +0100

    Fix CTRL+C not closing the program

--- src/preprocessor/gui/main_window.py
+++ src/preprocessor/gui/main_window.py
@@ -1,4 +1,5 @@
 import logging
+import signal
 import sys
 from typing import cast, Callable
 
@@ -319,6 +320,26 @@ class MainWindow(QMainWindow):
         QtCore.QTimer.singleShot(0, apply)
         logger.debug("Scheduled display on UI")
 
+def sigint_handler(*args):
+    """Handler for the SIGINT signal."""
+    sys.stderr.write('\r')
+    QApplication.quit()
+
+signal_timer: QTimer
+
+def setup_sigint_handler(interval: int = 200) -> None:
+    """Process any pending SIGINT signals."""
+
+    # Based on: https://stackoverflow.com/a/4939113/146622
+    # The Qt application main event loop doesn't run on the Python interpreter, so it doesn't process signals.
+    # Setup timer to periodically run the Python interpreter to check for (SIGINT) signals from outside.
+
+    global signal_timer  # Prevent garbage collection
+
+    signal.signal(signal.SIGINT, sigint_handler)
+    signal_timer = QTimer()
+    signal_timer.start(interval)
+    signal_timer.timeout.connect(lambda: None)  # Let the interpreter run each time.
 
 def show_application() -> None:
     """Show the main application window."""
@@ -329,5 +350,6 @@ def show_application() -> None:
     app.setApplicationVersion(__version__)
     window = MainWindow()
     window.show()
+    setup_sigint_handler()
     exit_code = app.exec_()
     sys.exit(exit_code)
