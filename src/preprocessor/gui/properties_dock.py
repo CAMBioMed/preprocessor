82332fb168b3e4ee44919ef1ab41f3887da3fdcf
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2025-11-05 17:12:44 +0100

    Add property tabs

--- src/preprocessor/gui/properties_dock.py
+++ src/preprocessor/gui/properties_dock.py
@@ -15,75 +15,105 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDockWidget, QFormLayout, QLabel,
-    QSizePolicy, QSlider, QToolBox, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QDockWidget, QFormLayout, QGridLayout,
+    QGroupBox, QLabel, QScrollArea, QSizePolicy,
+    QVBoxLayout, QWidget)
 
 class Ui_PropertiesDock(object):
     def setupUi(self, PropertiesDock):
         if not PropertiesDock.objectName():
             PropertiesDock.setObjectName(u"PropertiesDock")
-        PropertiesDock.resize(400, 300)
+        PropertiesDock.resize(400, 362)
         self.dockWidgetContents = QWidget()
         self.dockWidgetContents.setObjectName(u"dockWidgetContents")
-        self.verticalLayout = QVBoxLayout(self.dockWidgetContents)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.toolBox = QToolBox(self.dockWidgetContents)
-        self.toolBox.setObjectName(u"toolBox")
-        self.page1 = QWidget()
-        self.page1.setObjectName(u"page1")
-        self.page1.setGeometry(QRect(0, 0, 382, 198))
-        self.formLayout_2 = QFormLayout(self.page1)
-        self.formLayout_2.setObjectName(u"formLayout_2")
-        self.label = QLabel(self.page1)
+        self.gridLayout = QGridLayout(self.dockWidgetContents)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.scrollArea = QScrollArea(self.dockWidgetContents)
+        self.scrollArea.setObjectName(u"scrollArea")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.scrollArea.sizePolicy().hasHeightForWidth())
+        self.scrollArea.setSizePolicy(sizePolicy)
+        self.scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        self.scrollAreaWidgetContents = QWidget()
+        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 366, 240))
+        self.verticalLayout_2 = QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.groupBox = QGroupBox(self.scrollAreaWidgetContents)
+        self.groupBox.setObjectName(u"groupBox")
+        self.formLayout = QFormLayout(self.groupBox)
+        self.formLayout.setObjectName(u"formLayout")
+        self.label = QLabel(self.groupBox)
         self.label.setObjectName(u"label")
 
-        self.formLayout_2.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label)
+        self.formLayout.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label)
 
-        self.horizontalSlider = QSlider(self.page1)
-        self.horizontalSlider.setObjectName(u"horizontalSlider")
-        self.horizontalSlider.setMaximum(500)
-        self.horizontalSlider.setValue(50)
-        self.horizontalSlider.setOrientation(Qt.Orientation.Horizontal)
 
-        self.formLayout_2.setWidget(0, QFormLayout.ItemRole.FieldRole, self.horizontalSlider)
+        self.verticalLayout_2.addWidget(self.groupBox)
 
-        self.label_2 = QLabel(self.page1)
+        self.groupBox_2 = QGroupBox(self.scrollAreaWidgetContents)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.formLayout_2 = QFormLayout(self.groupBox_2)
+        self.formLayout_2.setObjectName(u"formLayout_2")
+        self.label_2 = QLabel(self.groupBox_2)
         self.label_2.setObjectName(u"label_2")
 
-        self.formLayout_2.setWidget(1, QFormLayout.ItemRole.LabelRole, self.label_2)
+        self.formLayout_2.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label_2)
 
-        self.horizontalSlider_2 = QSlider(self.page1)
-        self.horizontalSlider_2.setObjectName(u"horizontalSlider_2")
-        self.horizontalSlider_2.setMaximum(500)
-        self.horizontalSlider_2.setValue(150)
-        self.horizontalSlider_2.setOrientation(Qt.Orientation.Horizontal)
 
-        self.formLayout_2.setWidget(1, QFormLayout.ItemRole.FieldRole, self.horizontalSlider_2)
+        self.verticalLayout_2.addWidget(self.groupBox_2)
 
-        self.toolBox.addItem(self.page1, u"Values")
-        self.page2 = QWidget()
-        self.page2.setObjectName(u"page2")
-        self.page2.setGeometry(QRect(0, 0, 382, 198))
-        self.toolBox.addItem(self.page2, u"Extra")
+        self.groupBox_3 = QGroupBox(self.scrollAreaWidgetContents)
+        self.groupBox_3.setObjectName(u"groupBox_3")
+        self.formLayout_3 = QFormLayout(self.groupBox_3)
+        self.formLayout_3.setObjectName(u"formLayout_3")
+        self.label_3 = QLabel(self.groupBox_3)
+        self.label_3.setObjectName(u"label_3")
 
-        self.verticalLayout.addWidget(self.toolBox)
+        self.formLayout_3.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label_3)
+
+
+        self.verticalLayout_2.addWidget(self.groupBox_3)
+
+        self.groupBox_4 = QGroupBox(self.scrollAreaWidgetContents)
+        self.groupBox_4.setObjectName(u"groupBox_4")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.groupBox_4.sizePolicy().hasHeightForWidth())
+        self.groupBox_4.setSizePolicy(sizePolicy1)
+        self.formLayout_4 = QFormLayout(self.groupBox_4)
+        self.formLayout_4.setObjectName(u"formLayout_4")
+        self.label_4 = QLabel(self.groupBox_4)
+        self.label_4.setObjectName(u"label_4")
+
+        self.formLayout_4.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label_4)
+
+
+        self.verticalLayout_2.addWidget(self.groupBox_4)
+
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.gridLayout.addWidget(self.scrollArea, 0, 0, 1, 1)
 
         PropertiesDock.setWidget(self.dockWidgetContents)
 
         self.retranslateUi(PropertiesDock)
-
-        self.toolBox.setCurrentIndex(0)
-
 
         QMetaObject.connectSlotsByName(PropertiesDock)
     # setupUi
 
     def retranslateUi(self, PropertiesDock):
         PropertiesDock.setWindowTitle(QCoreApplication.translate("PropertiesDock", u"Properties", None))
-        self.label.setText(QCoreApplication.translate("PropertiesDock", u"V1", None))
-        self.label_2.setText(QCoreApplication.translate("PropertiesDock", u"V2", None))
-        self.toolBox.setItemText(self.toolBox.indexOf(self.page1), QCoreApplication.translate("PropertiesDock", u"Values", None))
-        self.toolBox.setItemText(self.toolBox.indexOf(self.page2), QCoreApplication.translate("PropertiesDock", u"Extra", None))
+        self.groupBox.setTitle(QCoreApplication.translate("PropertiesDock", u"GroupBox", None))
+        self.label.setText(QCoreApplication.translate("PropertiesDock", u"TextLabel", None))
+        self.groupBox_2.setTitle(QCoreApplication.translate("PropertiesDock", u"GroupBox", None))
+        self.label_2.setText(QCoreApplication.translate("PropertiesDock", u"TextLabel", None))
+        self.groupBox_3.setTitle(QCoreApplication.translate("PropertiesDock", u"GroupBox", None))
+        self.label_3.setText(QCoreApplication.translate("PropertiesDock", u"TextLabel", None))
+        self.groupBox_4.setTitle(QCoreApplication.translate("PropertiesDock", u"GroupBox", None))
+        self.label_4.setText(QCoreApplication.translate("PropertiesDock", u"TextLabel", None))
     # retranslateUi
 
