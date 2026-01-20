9615889decf29fee3e12e737dbac62f5c7da1e12
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2026-01-20 12:49:22 +0100

    Setup platform-independent keyboard shortcuts

--- src/preprocessor/gui/main_window2.py
+++ src/preprocessor/gui/main_window2.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QCloseEvent, QAction
+from PySide6.QtGui import QCloseEvent, QAction, QKeySequence
 from PySide6.QtWidgets import QMainWindow, QWidget
 
 from preprocessor.gui.about_dialog import show_about_dialog
@@ -25,18 +25,50 @@ class MainWindow2(QMainWindow):
         super().__init__(parent)
         self.ui = Ui_Main()
         self.ui.setupUi(self)
+        self._setupKeyboardShortcuts()
+        self._create_thumbnail_dock()
 
         self.model = ApplicationModel()
         self._connect_signals()
 
         self.read_settings()
 
+    def _setupKeyboardShortcuts(self) -> None:
+        """
+        Set up keyboard shortcuts for actions.
+
+        As Qt Creator/Designer does not support setting StandardKey shortcuts,
+        we do it manually here. See:
+        https://forum.qt.io/topic/6259/qt-designer-does-not-support-qkeysequence-standardkey
+        """
+        # File menu
+        self.ui.menuFile_NewProject.setShortcut(QKeySequence.StandardKey.New)
+        self.ui.menuFile_OpenProject.setShortcut(QKeySequence.StandardKey.Open)
+        self.ui.menuFile_SaveProject.setShortcut(QKeySequence.StandardKey.Save)
+        self.ui.menuFile_SaveProjectAs.setShortcut(QKeySequence.StandardKey.SaveAs)
+        self.ui.menuFile_Exit.setShortcut(QKeySequence.StandardKey.Quit)
+
+        # Help menu
+        self.ui.menuHelp_About.setShortcut(QKeySequence.StandardKey.HelpContents)
+
+    def _create_thumbnail_dock(self) -> None:
+        """Create the thumbnail list dock widget."""
+        self.thumbnail_dock = ThumbnailListWidget()
+        self.thumbnail_dock.setAllowedAreas(
+            Qt.DockWidgetArea.TopDockWidgetArea | Qt.DockWidgetArea.BottomDockWidgetArea
+        )
+        self.addDockWidget(Qt.DockWidgetArea.BottomDockWidgetArea, self.thumbnail_dock)
+
     def _connect_signals(self) -> None:
+        """Connect signals to slots."""
         # File menu
         self.ui.menuFile_Exit.triggered.connect(self.close)
 
         # Help menu
         self.ui.menuHelp_About.triggered.connect(self.on_help_about)
+
+        # When a thumbnail is selected, either show stored result or schedule processing
+        # self.thumbnail_dock.on_thumbnail_selected.connect(self._display_image)
 
     def on_help_about(self) -> None:
         show_about_dialog(self)
