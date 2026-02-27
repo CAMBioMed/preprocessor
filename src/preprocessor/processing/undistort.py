2690c1b9ce2a9a3f3a6193d308fee7b7eb34d850
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2026-02-27 15:44:20 +0100

    Fix lens distort

--- src/preprocessor/processing/undistort.py
+++ src/preprocessor/processing/undistort.py
@@ -26,10 +26,12 @@ def undistort_photo(
     the operation will be aborted and None returned.
     """
     # Try to obtain camera/distortion parameters
-    cam = getattr(photo, "camera_matrix", None)
-    dist = getattr(photo, "distortion_coefficients", None)
-    if cam is None or dist is None:
-        return None
+    cam = photo.camera_matrix or (
+        (float(photo.width), 0, float(photo.width) / 2),
+        (0, float(photo.width), float(photo.height) / 2),
+        (0, 0, 1),
+    )
+    dist = photo.distortion_coefficients or [0.0, 0.0, 0.0, 0.0, 0.0]
 
     # Load the image from disk
     original_path = project.get_absolute_path(photo.original_filename)
